6bc9b01de1ac26b29dbae20a9ee6c520d815a662
DennisKnol <dennisknol94@gmail.com>
2019-04-08 15:30:41 +0200

    Add plot

--- Assignment_1/Task_2/visualize_class.py
+++ Assignment_1/Task_2/visualize_class.py
@@ -6,17 +6,23 @@ test_data = pd.read_csv("test.csv")
 
 plt.figure(figsize=(14,14))
 
-plt.subplot2grid((2, 2), (0, 0), colspan=2)
+plt.subplot2grid((3, 2), (0, 0), colspan=2)
 train_data.Pclass.value_counts().sort_index().plot(kind="bar", alpha=0.5)
 plt.title("Class of Travel")
 
-plt.subplot2grid((2, 2), (1, 0), colspan=2)
+plt.subplot2grid((3, 2), (1, 0), colspan=2)
 [train_data.Survived[train_data.Pclass == i].plot.kde(bw_method=0.3) for i in [1, 2, 3]]
 plt.legend(["First class", "Second class", "Third class"])
 plt.title("Density plot Survived wrt Class of Travel")
+
+plt.subplot2grid((3, 2), (2, 0), colspan=2)
+[train_data.Fare[train_data.Pclass == i].plot.kde(bw_method=0.3) for i in [1, 2, 3]]
+plt.legend(["First class", "Second class", "Third class"])
+plt.title("Density plot Fare wrt Class of Travel")
 
 plt.show()
 
 # to conclude:
 # We see a clear difference in survival rates in the different classes
 # Survival rates increases from the third class to the first
+# Also, fare increases from the third class to the first
