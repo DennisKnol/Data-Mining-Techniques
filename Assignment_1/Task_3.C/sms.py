028fd5ac644ce7b893a587efe7464dbe358132f7
DennisKnol <dennisknol94@gmail.com>
2019-04-14 18:21:02 +0200

    ...

--- Assignment_1/Task_3.C/sms.py
+++ Assignment_1/Task_3.C/sms.py
@@ -2,9 +2,15 @@ import pandas as pd
 import csv
 
 
+sms_data = []
 with open("SmsCollection.csv", 'r') as file:
     reader = csv.reader(file, delimiter=';')
     for row in reader:
-        print(row)
+        sms_data.append(row)
+
+sms_data = sms_data[:-1]
+
+
+
 
 
