f63de448ecad5391a89d67a9db4d9c63c11cd8cd
DennisKnol <dennisknol94@gmail.com>
2019-04-16 15:45:25 +0200

    Task 3.B done

--- Assignment_1/Task_3.B/MSE_vs_MAE.py
+++ Assignment_1/Task_3.B/MSE_vs_MAE.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pylab as plt
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, ridge_regression
+from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 df = pd.read_csv('Consumo_cerveja.csv')
@@ -44,18 +43,35 @@ corr_mat = df.corr().round(4)
 
 # visualize the prepared data
 cols = ["consumption", "mean_temp", "min_temp", "max_temp", "precipitation", "weekend"]
-
 sns.set()
-sns.pairplot(df[cols], kind='reg')
+sns.pairplot(df, y_vars=cols[0], x_vars=cols[1:6], kind='reg')
 plt.show()
 
+plt.figure(figsize=(18, 5))
+plt.subplot2grid((1, 3), (0, 0))
+sns.boxplot(x='weekend', y='consumption', data=df, orient='v', width=0.5)
+plt.title("Beer Consumption")
+plt.xlabel("Weekend")
+plt.ylabel("Liters")
+
+plt.subplot2grid((1, 3), (0, 1))
 df["consumption"].plot()
+plt.title("Beer Consumption")
+plt.ylabel("Liters")
+plt.xlabel("Days")
+
+plt.subplot2grid((1, 3), (0, 2))
+df["max_temp"].plot()
+plt.title("Maximum Temperature")
+plt.ylabel("Degrees Celsius")
+plt.xlabel("Days")
+
 plt.show()
+
 
 # prepared data divided in X and y
 y = df["consumption"]
 X = df[["max_temp", "precipitation", "weekend"]]
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 lin_regr = LinearRegression()
@@ -63,15 +79,43 @@ lin_regr.fit(X_train, y_train)
 
 score_linear = lin_regr.score(X_train, y_train)
 y_pred_linear = lin_regr.predict(X_test)
-pred_score = r2_score(y_test, y_pred_linear)
+pred_score_linear = r2_score(y_test, y_pred_linear)
 
-# X = df[[ "min_temp", "max_temp", "precipitation", "weekend"]]
-#
+mse_linear = mean_squared_error(y_test, y_pred_linear)
+mae_linear = mean_absolute_error(y_test, y_pred_linear)
+
+# define different set of explanatory and dependent variable for the logistic regression
+y = df["weekend"]
+X = df[["consumption", "mean_temp"]]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+log_regr = LogisticRegression()
+log_regr.fit(X_train, y_train)
+
+score_log = log_regr.score(X_train, y_train)
+y_pred_log = log_regr.predict(X_test)
+pred_score_log = r2_score(y_test, y_pred_log)
+
+mse_log = mean_squared_error(y_test, y_pred_log)
+mae_log = mean_absolute_error(y_test, y_pred_log)
+
+# results
+print("MSE of linear regression = ", round(mse_linear, 4), '\n')
+print("MAE of linear regression = ", round(mae_linear, 4), '\n')
+print("MSE of logistic regression = ", round(mse_log, 4), '\n')
+print("MAE of logistic regression = ", round(mae_log, 4))
+
+# # define different set of explanatory variables for the ridge regression
+# y = df["consumption"]
+# X = df[["min_temp", "max_temp", "precipitation", "weekend"]]
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 #
-# ridge_regr = ridge_regression()
+# ridge_regr = Ridge()
 # ridge_regr.fit(X_train, y_train)
 #
 # score_ridge = ridge_regr.score(X_train, y_train)
 # y_pred_ridge = ridge_regr.predict(X_test)
-
+# pred_score_ridge = r2_score(y_test, y_pred_ridge)
+#
+# mse_ridge = mean_squared_error(y_test, y_pred_ridge)
+# mae_ridge = mean_absolute_error(y_test, y_pred_ridge)
