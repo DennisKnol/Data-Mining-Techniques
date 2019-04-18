0412264c0c0a2761ecc940b6ede2922bcdf64982
DennisKnol <dennisknol94@gmail.com>
2019-04-18 20:55:48 +0200

    data_prep done

--- Assignment_1/Task_2/data_prep.py
+++ Assignment_1/Task_2/data_prep.py
@@ -1,17 +1,6 @@
 import pandas as pd
 import numpy as np
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
-from sklearn.neural_network import MLPClassifier
-
 
 train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
@@ -126,39 +115,9 @@ def prep_data(data):
 
 # train_data_outlier_removed = remove_outlier(train_data)
 # print(train_data_outlier_removed.shape)
-train_data_prepped = prep_data(train_data)
 
-# sns.set()
-# cols = ["Survived", "Pclass", "Sex", "SibSp", "Parch", "Cabin", "Embarked", "Fare", "Age"]
-# sns.pairplot(train_data[cols], height=2.5)
-# plt.show()
-
-
-y = train_data_prepped["Survived"]
-X = train_data_prepped[["Pclass", "Sex", "SibSp", "Parch", "Cabin", "Embarked", "FareBins", "AgeCategories", "Title"]]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
-
-models = [
-    DecisionTreeClassifier(),
-    KNeighborsClassifier(),
-    GradientBoostingClassifier(),
-    SVC(),
-    MLPClassifier(),
-    RandomForestClassifier()
-]
-
-for model in models:
-    model.fit(X_train, y_train)
-    score = round(model.score(X_test, y_test), 2)
-    print(score)
+# train_data_prepped = prep_data(train_data)
+# train_data_prepped.to_csv("train_prep.csv")
 
 test_data_prepped = prep_data(test_data)
-X = test_data_prepped[["Pclass", "Sex", "SibSp", "Parch", "Cabin", "Embarked", "FareBins", "AgeCategories", "Title"]]
-
-gbk = GradientBoostingClassifier()
-gbk.fit(X_train, y_train)
-prediction_survived = pd.DataFrame(gbk.predict(X))
-submission = pd.concat([test_data["PassengerId"], prediction_survived], axis=1)
-submission.columns = ["PassengerId", "Survived"]
-submission.to_csv('survived_submission.csv', index=False)
+test_data_prepped.to_csv("test_prep.csv")
