defb604806e782e2e7a65ebd51c8d5bba3799ab0
DennisKnol <dennisknol94@gmail.com>
2019-04-18 13:49:55 +0200

    added plots

--- Assignment_1/Task_1/data_visualization.py
+++ Assignment_1/Task_1/data_visualization.py
@@ -18,6 +18,9 @@ odi = pd.read_csv(
 )
 
 odi = odi.drop(odi.columns[0], axis=1)
+print(odi.info())
+
+corr_mat = odi.corr().round(4)
 
 plt.figure(figsize=(15, 8))
 odi["programme"].value_counts().plot(
@@ -29,6 +32,7 @@ plt.show()
 
 print(round(odi["gender"].value_counts(normalize=True), 2))
 
+# Pie plots for multiple choice answers
 plt.figure(figsize=(18, 8))
 
 plt.subplot2grid((2, 3), (0, 0))
@@ -93,3 +97,61 @@ plt.axis('off')
 
 plt.show()
 
+# room capacity is 343, meaning that the max numbers is 342
+# https://www.vu.nl/nl/Images/Zaalfaciliteiten_aug2018_tcm289-257362.pdf
+odi["number_of_neighbors"] = odi["number_of_neighbors"].dropna()
+odi["number_of_neighbors"] = odi["number_of_neighbors"].drop(
+    odi["number_of_neighbors"][odi["number_of_neighbors"] > 342].index
+)
+
+# random number was supposed to be between 0 and 10
+odi["random_number"] = odi["random_number"].dropna()
+odi["random_number"] = odi["random_number"].drop(
+    odi["random_number"][odi["random_number"] > 10].index
+)
+
+# 100 euro is the limit
+odi["deserve_money"] = odi["deserve_money"].dropna()
+odi["deserve_money"] = odi["deserve_money"].drop(
+    odi["deserve_money"][odi["deserve_money"] > 100].index
+)
+
+plt.figure(figsize=(18, 8))
+
+plt.subplot2grid((2, 4), (0, 0))
+sns.distplot(odi["number_of_neighbors"], bins=100)
+plt.title("Density plot of the number of neighbors")
+
+plt.subplot2grid((2, 4), (1, 0))
+sns.barplot(x="gender", y="number_of_neighbors", data=odi, alpha=0.8, palette=['lightblue', 'pink', 'beige'])
+plt.title("Mean number of neighbors per Gender")
+plt.ylabel("Mean number of neighbors")
+
+plt.subplot2grid((2, 4), (0, 1))
+sns.distplot(odi["random_number"], bins=100)
+plt.title("Density plot of random number between 0 an 10")
+
+plt.subplot2grid((2, 4), (1, 1))
+sns.barplot(x="gender", y="random_number", data=odi, alpha=0.8, palette=['lightblue', 'pink', 'beige'])
+plt.title("Mean number of neighbors per Gender")
+plt.ylabel("Mean number of neighbors")
+
+plt.subplot2grid((2, 4), (0, 2))
+sns.distplot(odi["deserve_money"], bins=100)
+plt.title("Density plot of the amount of money student think they deserve")
+
+plt.subplot2grid((2, 4), (1, 2))
+sns.barplot(x="gender", y="deserve_money", data=odi, alpha=0.8, palette=['lightblue', 'pink', 'beige'])
+plt.title("Mean amount of money per Gender")
+plt.ylabel("Mean amount of money")
+
+plt.subplot2grid((2, 4), (0, 3))
+sns.distplot(odi["stress_level"], bins=100)
+plt.title("Density plot of the level of stress")
+
+plt.subplot2grid((2, 4), (1, 3))
+sns.barplot(x="gender", y="stress_level", data=odi, alpha=0.8, palette=['lightblue', 'pink', 'beige'])
+plt.title("Mean level of stress per Gender")
+plt.ylabel("Mean stress level")
+
+plt.show()
