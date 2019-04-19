692353e9f95d673d0eed70baa6401be1a3c0b310
DennisKnol <dennisknol94@gmail.com>
2019-04-19 18:47:03 +0200

    added logic to predict ham or spam

--- Assignment_1/Task_3.C/sms.py
+++ Assignment_1/Task_3.C/sms.py
@@ -1,16 +1,34 @@
 import pandas as pd
-import csv
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import svm
+from sklearn.metrics import r2_score
 
 
-sms_data = []
-with open("SmsCollection.csv", 'r') as file:
-    reader = csv.reader(file, delimiter=';')
-    for row in reader:
-        sms_data.append(row)
+df = pd.read_csv('SmsCollection.csv', delimiter=';', usecols=[0, 1])
 
-sms_data = sms_data[:-1]
+# convert to 0 and 1
+df.loc[df["label"] == "ham", "label"] = 0
+df.loc[df["label"] == "spam", "label"] = 1
+df["label"] = df["label"].astype('int')
 
+print(df["label"].value_counts(normalize=True))
+print(df.isnull().sum())
 
+df = df.dropna()
 
+# convert to lowercase
+df["text"] = df["text"].str.lower()
 
+X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=0.1, random_state=1)
 
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train)
+
+svm = svm.SVC(C=1000)
+svm.fit(X_train, y_train)
+
+X_test = vectorizer.transform(X_test)
+y_pred = svm.predict(X_test)
+print(r2_score(y_test, y_pred))
