c069e7fe6118714873c5f3116a5a4cc665d63ccb
DennisKnol <dennisknol94@gmail.com>
2019-05-02 19:05:35 +0200

    Added file for preparation of data

---
+++ Assignment_2/prep_example_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from functions import missing_values, convert_date_time, combine_competitors
+
+
+df = pd.read_csv("df_sample.csv")
+df = df.drop(df.columns[0], axis=1)
+
+comp_rate = [df["comp{}_rate".format(i)] for i in range(1, 9)]
