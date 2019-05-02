034f8d023647a122786309ac4e6f6a20a143b2cd
DennisKnol <dennisknol94@gmail.com>
2019-05-02 13:23:48 +0200

    ..

--- Assignment_2/explore_sample_data.py
+++ Assignment_2/explore_sample_data.py
@@ -9,9 +9,13 @@ df = pd.read_csv("df_sample.csv")
 df = df.drop(df.columns[0], axis=1)
 
 print(df.info())
-print("Shape of the dataframe: ", df.shape, "\n")  # (4958347, 54)
+print("Shape of the dataframe: ", df.shape, "\n")  # (1000, 54)
 print("Sum of is null values: \n", df.isnull().sum(), "\n")
 print("Sum of NaN values:\n ", df.isna().sum())
 
+
+
+
 df[["date", "time"]] = df["date_time"].str.split(" ", expand=True)
 df[["year", "month", "day"]] = df["date"].str.split("-", expand=True)
+
--- Assignment_2/process_report.md
+++ Assignment_2/process_report.md
@@ -3,3 +3,6 @@ Process Report
 23-04:
 - Read in data and initial exploration
 
+02-05:
+- sample dataframe
+= 
